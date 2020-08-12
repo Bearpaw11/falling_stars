d95a6d9684e5f2f335340eabe854c4a30c9c5839
Chris Behrens <chrisbehrens@yahoo.com>
2020-08-12 08:15:32 -0700

    updated font colors in Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,10 +14,10 @@ function Navmenu() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto navbar">
-            <Nav.Link href="/">Home </Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link className="link"href="/">Home </Nav.Link>
+            <Nav.Link className="link"href="/about">About</Nav.Link>
+            <Nav.Link className="link"href="/projects">Projects</Nav.Link>
+            <Nav.Link className="link"href="/contact">Contact</Nav.Link>
         </Nav>
         </Navbar.Collapse>
         </Navbar>
