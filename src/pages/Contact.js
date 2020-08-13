1729dd1d62ea802e58e322b0f80d36bcd251aa56
Chris Behrens <chrisbehrens@yahoo.com>
2020-08-12 20:38:41 -0700

    added instgram link on COntact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
 function Contact (){
+    function instaRedirect() {
+        const url = "https://www.instagram.com/fallingstarsdesign/"
+        window.open(url, "_blank")
+    }
+
     return (
         <div>
             <p className='design'></p>
             <div className= 'contact'>
             <h3>Amber R. Behrens, Owner/Chief Designer</h3>
-            <h3><a class="mailto" href="mailto:fallingstarsdesign@gmail.com">fallingstarsdesign@gmail.com</a></h3>
-            <h3>@fallingstarsdesign</h3>
+            <h3><a className="mailto" href="mailto:fallingstarsdesign@gmail.com">fallingstarsdesign@gmail.com</a></h3>
+            <h3 className='insta1'onClick={instaRedirect}>@fallingstarsdesign</h3>
             <h3>480.280.2566</h3>
             </div>
         </div>
